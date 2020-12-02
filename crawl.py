2ac4b7beafa8066d8e656da8acc9ff937282f335
Roc-Zhang <39946396+Roc-Zhang@users.noreply.github.com>
2020-12-02 21:09:35 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,30 +1,45 @@
 from threading import Thread
-from Queue import Queue
+from queue import Queue
 import requests
-headers={'User-Agent': 'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
+from lxml import etree
+import json
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
 q=Queue()
-base_url="https://www.qiushibaike.com/text/{}"
+base_url = r"https://www.qiushibaike.com/text/{}"
+proxies={"https":"https://61.160.210.223:808"}
 for i in range(50):
     q.put(base_url.format(i+1))
 
 
 class Mythread(Thread):
-    def __init__(self,name,q):
+    def __init__(self, q):
         super(Mythread,self).__init__()
-        self.name=name
         self.url_queue=q
+
 
     def run(self):
         while not self.url_queue.empty():
-            url=self.url_queue.get()
-            response=requests.get(url,headers=headers)
-            print("%s getting url:%s content \n" %(self.name,url))
+            url = self.url_queue.get()
+            response = requests.get(url,headers=headers,proxies=proxies)    
+            html=etree.HTML(response.text, etree.HTMLParser())
+            content=html.xpath("//div[@class='content']/span/text()")
+            print(content)
+            
 
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     for i in range(1,4):
-        mythread=Mythread("thread{}".format(i),q)
+        mythread = Mythread(q)
         mythread.start()
+
+    
+
+        
+        
+        
+
 
 
         
