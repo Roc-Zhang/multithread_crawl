c9a6a864f5f512c2dbed33174ff9644fa82948a3
Roc-Zhang <39946396+Roc-Zhang@users.noreply.github.com>
2020-12-03 21:48:53 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -2,13 +2,13 @@ from threading import Thread
 from queue import Queue
 import requests
 from lxml import etree
-import json
+import proxy_ip
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
 q=Queue()
 base_url = r"https://www.qiushibaike.com/text/{}"
-proxies={"https":"https://61.160.210.223:808"}
+proxies=proxy_ip.get_ip()
 for i in range(50):
     q.put(base_url.format(i+1))
 
