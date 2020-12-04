4f3e2e94d825ec46865bafb47d0e7d39c6d529b8
Roc-Zhang <39946396+Roc-Zhang@users.noreply.github.com>
2020-12-04 22:11:48 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -4,20 +4,21 @@ import requests
 from lxml import etree
 import proxy_ip
 import json
+import time
+
 
 lock=Lock()
 headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
 q=Queue()
 base_url = r"https://www.qiushibaike.com/text/{}"
 proxies=proxy_ip.get_ip()
-for i in range(50):
+for i in range(10):
     q.put(base_url.format(i+1))
 
 
 class Mythread(Thread):
-    def __init__(self, q,num):
+    def __init__(self,  num):
         super(Mythread,self).__init__()
-        self.url_queue=q
         self.num=num
 
 
@@ -27,30 +28,29 @@ class Mythread(Thread):
             print(".",end="",flush=True)
             time.sleep(1)
         print("\n",end="")
-        while not self.url_queue.empty():
-            url = self.url_queue.get()
-            response = requests.get(url,headers=headers,proxies=proxies)    
-            html=etree.HTML(response.text, etree.HTMLParser())
-            content=html.xpath("//div[@class='content']/span/text()")
-            lock.acquire()
-            with open("./text.txt","a+") as f:
-                f.write(json.dumps(content))
+        url = q.get()
+        response = requests.get(url,headers=headers,proxies=proxies)    
+        html=etree.HTML(response.text, etree.HTMLParser())
+        content=html.xpath("//div[@class='content']/span/text()")
+        
+        lock.acquire()
+        with open("./text.txt","w") as f:
+            for content in contents:
+                f.write(json.dumps(content,ensure_ascii=False))
                 f.write("\n")
-            lock.release()
-            
-            
+                print(content)
+        print(content)
+
+
+
 if __name__ == "__main__":
-    for i in range(1,4):
-        mythread = Mythread(q,i)
+    for i in range(q.qsize()):
+        mythread = Mythread(i+1)
         mythread.start()
+        mythread.join()
+        
 
     
-
-        
-        
-        
-
-
 
         
         
