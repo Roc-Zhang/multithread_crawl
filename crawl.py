61bca656b9a2918cd373bf6782a477409579ba16
Roc-Zhang <39946396+Roc-Zhang@users.noreply.github.com>
2020-12-04 16:55:21 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,10 +1,11 @@
-from threading import Thread
+from threading import Thread, Lock
 from queue import Queue
 import requests
 from lxml import etree
 import proxy_ip
+import json
 
-
+lock=Lock()
 headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
 q=Queue()
 base_url = r"https://www.qiushibaike.com/text/{}"
@@ -14,23 +15,33 @@ for i in range(50):
 
 
 class Mythread(Thread):
-    def __init__(self, q):
+    def __init__(self, q,num):
         super(Mythread,self).__init__()
         self.url_queue=q
+        self.num=num
 
 
     def run(self):
+        print("MyThread%d is running" %self.num,end="")
+        for i in range(3):     #每隔3秒爬取一次，免的太快让人家觉得不爽！！！
+            print(".",end="",flush=True)
+            time.sleep(1)
+        print("\n",end="")
         while not self.url_queue.empty():
             url = self.url_queue.get()
             response = requests.get(url,headers=headers,proxies=proxies)    
             html=etree.HTML(response.text, etree.HTMLParser())
             content=html.xpath("//div[@class='content']/span/text()")
-            print(content)
+            lock.acquire()
+            with open("./text.txt","a+") as f:
+                f.write(json.dumps(content))
+                f.write("\n")
+            lock.release()
             
-
+            
 if __name__ == "__main__":
     for i in range(1,4):
-        mythread = Mythread(q)
+        mythread = Mythread(q,i)
         mythread.start()
 
     
